b696f3c6d900cab48513168d0ccc10a19903fbb9
jiachongliu <jcl314159@gmail.com>
2017-09-13 23:48:31 +0800

    build deeplearning
    build weight_variable

--- deeplearning.py
+++ deeplearning.py
@@ -25,8 +25,21 @@ BATCH            = 32
 FRAME_PER_ACTION = 1
 
 
-def weight_variable(shape);
-def bias_variable(shape);
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev = 0.01)
+    """
+    truncated_normal(shape,mean,stddev) -- 这个函数是用来产生正太分布,均值为mean,但在这个函数中没有使用，缺省值
+    shape(生成张量的纬度) stddev(标准差)
+
+    """
+    return tf.Variable(intial)   #Variable(变量)  设定变量
+
+def bias_variable(shape):
+    inital = tf.constant(0.01, shape = shape)
+    """
+
+    """
+    return tf.Variable(inital)
 def conv2d(x, W, stride);
 def max_pool_2x2(x);
 def createNetwork();
